d61014c84b0cfef0548628fd5607bde316156dda
이현세 <dlgustp1487@naver.com>
2022-04-22 10:57:58 +0900

    리팩토링1. 함수 선언 변경하기

---
+++ src/main/java/hello/refactoring/_01_smell_mysterious_name/before/StudyDashBoard.java
@@ -0,0 +1,54 @@
+package hello.refactoring._01_smell_mysterious_name.before;
+
+
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+public class StudyDashBoard {
+
+	
+	private Set<String> usernames = new HashSet<>();
+	private Set<String> reviews = new HashSet<>();
+	
+	/**
+	 * 스터리 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
+	 * @param issue
+	 * @throws IOException
+	 */
+
+	private void loadReviews(GHIssue issue) throws IOException {
+		List<GHIssueComment> comments = issue.getComments();
+		for (GHIssueComment comment : comments) {
+			usernames.add(comment.getUserName());
+			reviews.add(comment.getBody());
+			
+		}
+	}
+	
+	public Set<String> getUsernames() {
+		return usernames;
+	}
+	
+	public Set<String> getReviews() {
+		return reviews;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		GitHub gitHub = GitHub.connect();
+		GHRepository repository = gitHub.getRepository("whiteship/live-study");
+		GHIssue issue = repository.getIssue(30);
+		
+		StudyDashBoard studyDashBoard = new StudyDashBoard();
+		studyDashBoard.loadReviews(issue);
+		studyDashBoard.getUsernames().forEach(System.out::println);
+		studyDashBoard.getReviews().forEach(System.out::println);
+	}
+}
