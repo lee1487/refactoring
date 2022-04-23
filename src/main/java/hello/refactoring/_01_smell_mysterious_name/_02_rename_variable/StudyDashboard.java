2014ca947c841a04baf750d731a6f52cfbe1f388
hyeonse <dlgustp1487@naver.com>
2022-04-23 16:31:43 +0900

    리팩토링 3. 필드 이름 바꾸기

--- src/main/java/hello/refactoring/_01_smell_mysterious_name/_02_rename_variable/StudyDashboard.java
+++ src/main/java/hello/refactoring/_01_smell_mysterious_name/_02_rename_variable/StudyDashboard.java
@@ -5,6 +5,8 @@ import org.kohsuke.github.GHIssueComment;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
+import hello.refactoring._01_smell_mysterious_name._03_rename_field.StudyReview;
+
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
@@ -12,9 +14,7 @@ import java.util.Set;
 
 public class StudyDashboard {
 
-    private Set<String> usernames = new HashSet<>();
-
-    private Set<String> reviews = new HashSet<>();
+    private Set<StudyReview> studyReviews = new HashSet<>();
 
     /**
      * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
@@ -25,25 +25,19 @@ public class StudyDashboard {
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
         GHIssue issue = repository.getIssue(30);
 
-        List<GHIssueComment> comments = issue.getComments();
-        for (GHIssueComment comment : comments) {
-            usernames.add(comment.getUserName());
-            this.reviews.add(comment.getBody());
+        List<GHIssueComment> reviews = issue.getComments();
+        for (GHIssueComment review : reviews) {
+            studyReviews.add(new StudyReview(review.getUserName(), review.getBody()));
         }
     }
 
-    public Set<String> getUsernames() {
-        return usernames;
-    }
-
-    public Set<String> getReviews() {
-        return reviews;
-    }
+    public Set<StudyReview> getStudyReviews() {
+		return studyReviews;
+	}
 
     public static void main(String[] args) throws IOException {
         StudyDashboard studyDashboard = new StudyDashboard();
         studyDashboard.loadReviews();
-        studyDashboard.getUsernames().forEach(name -> System.out.println(name));
-        studyDashboard.getReviews().forEach(review -> System.out.println(review));
+        studyDashboard.getStudyReviews().forEach(System.out::println);
     }
 }
