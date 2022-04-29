2725321e874f8e2991f32afae15e15357c472923
hyeonse <dlgustp1487@naver.com>
2022-04-29 20:57:57 +0900

    리팩토링 18. 변수 쪼개기

---
+++ src/test/java/hello/refactoring/_06_mutable_data/_18_split_variable/OrderTest.java
@@ -0,0 +1,18 @@
+package hello.refactoring._06_mutable_data._18_split_variable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderTest {
+
+	@Test
+	void distance() {
+		Order order = new Order();
+		assertEquals(50d, order.discount(50d, 100));
+		assertEquals(51d -2, order.discount(51d, 100));
+		assertEquals(50d -1, order.discount(50d, 101));
+		assertEquals(51d -2 -1, order.discount(51d, 101));
+	}
+
+}
