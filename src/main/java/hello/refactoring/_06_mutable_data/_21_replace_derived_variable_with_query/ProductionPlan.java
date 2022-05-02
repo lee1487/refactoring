9dca0b8bbfc89ccc24f76a0d9344ea45ead66dfe
hyeonse <dlgustp1487@naver.com>
2022-05-02 19:44:10 +0900

    리팩토링 21. 파생 변수를 질의 함수로 바꾸기

---
+++ src/main/java/hello/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
@@ -0,0 +1,24 @@
+package hello.refactoring._06_mutable_data._21_replace_derived_variable_with_query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductionPlan {
+
+//	private double production;
+	private List<Double> adjustments = new ArrayList<>();
+
+	public void applyAdjustment(double adjustment) {
+		this.adjustments.add(adjustment);
+//		this.production += adjustment;
+	}
+
+	public double getProduction() {
+		return this.adjustments.stream().mapToDouble(Double::valueOf).sum();
+	}
+
+//	private double calculatedProduction() {
+////		return this.adjustments.stream().reduce((double)0, (a, b) -> a+b);
+//		return this.adjustments.stream().mapToDouble(Double::valueOf).sum();
+//	}
+}
