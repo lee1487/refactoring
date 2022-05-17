08c5787a5d3cd9a1f2dd5cc8145db7da772a63f4
hyeonse <dlgustp1487@naver.com>
2022-05-17 20:33:08 +0900

    중간커밋

---
+++ src/main/java/hello/refactoring/_11_primitive_obsession/_30_repliace_primitive_with_object/after/Order.java
@@ -0,0 +1,16 @@
+package hello.refactoring._11_primitive_obsession._30_repliace_primitive_with_object.after;
+
+public class Order {
+
+	private String priority;
+
+	public Order(String priority) {
+		this.priority = priority;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+
+}
