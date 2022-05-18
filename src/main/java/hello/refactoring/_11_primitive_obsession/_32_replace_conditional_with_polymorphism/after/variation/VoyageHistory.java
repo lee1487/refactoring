5f78960a8792c2b19b53ac13549baf49f779c4bc
이현세 <dlgustp1487@naver.com>
2022-05-18 17:49:42 +0900

    리팩토링32. 중간커밋

---
+++ src/main/java/hello/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/after/variation/VoyageHistory.java
@@ -0,0 +1,5 @@
+package hello.refactoring._11_primitive_obsession._32_replace_conditional_with_polymorphism.after.variation;
+
+public record VoyageHistory(String zone, int profit) {
+
+}
---
+++ src/main/java/hello/refactoring/_11_primitive_obsession/_32_replace_conditional_with_polymorphism/before/variation/VoyageHistory.java
@@ -0,0 +1,5 @@
+package hello.refactoring._11_primitive_obsession._32_replace_conditional_with_polymorphism.before.variation;
+
+public record VoyageHistory(String zone, int profit) {
+
+}
