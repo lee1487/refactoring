859510b30502b7c69cebbf93831a8cd59d6aa143
hyeonse <dlgustp1487@naver.com>
2022-05-19 22:29:41 +0900

    리팩토링 41. 슈퍼클래스 추출하기

---
+++ src/test/java/hello/refactoring/_20_large_class/_41_extract_superclass/after/Employee.java
@@ -0,0 +1,22 @@
+package hello.refactoring._20_large_class._41_extract_superclass.after;
+
+public class Employee extends Party {
+
+	private Integer id;
+	private double monthlyCost;
+
+	public Employee(String name) {
+		super(name);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public double monthlyCost() {
+		return monthlyCost;
+	}
+
+
+}
