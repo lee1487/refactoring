8ce6428bcff7f91ff2358103cceecc05ecfcbbc9
hyeonse <dlgustp1487@naver.com>
2022-05-20 09:24:51 +0900

    리팩토링 42. 레코드 캡슐화하기

---
+++ src/main/java/hello/refactoring/_22_data_class/_42_encapsulate_record/after/Organization.java
@@ -0,0 +1,8 @@
+package hello.refactoring._22_data_class._42_encapsulate_record.after;
+
+public record Organization(String name, String country) {
+
+
+
+
+}
---
+++ src/test/java/hello/refactoring/_22_data_class/_42_encapsulate_record/after/OrganizationTest.java
@@ -0,0 +1,15 @@
+package hello.refactoring._22_data_class._42_encapsulate_record.after;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationTest {
+
+	@Test
+	void name() {
+		Organization organization = new Organization("hslee", "Korea");
+
+	}
+
+}
